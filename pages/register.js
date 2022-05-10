af82528ec62be3fa6890334ae920849d16c2e79d
Flashminat0 <wyasaslowe@gmail.com>
2022-05-10 16:11:11 +0530

    minor fix

--- pages/register.js
+++ pages/register.js
@@ -41,10 +41,8 @@ const Register = () => {
                         })
                     })
                     .catch((error) => {
-                        // An error occurred
-                        // ...
+                        console.log(error);
                     });
-                // ...
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -59,10 +57,10 @@ const Register = () => {
 
         signInWithPopup(auth, provider)
             .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
+
                 const credential = GoogleAuthProvider.credentialFromResult(result);
                 const token = credential.accessToken;
-                // The signed-in user info.
+
                 const user = result.user;
 
             })
@@ -78,14 +76,10 @@ const Register = () => {
             })
 
             .catch((error) => {
-                // Handle Errors here.
                 const errorCode = error.code;
                 const errorMessage = error.message;
-                // The email of the user's account used.
                 const email = error.email;
-                // The AuthCredential type that was used.
                 const credential = GoogleAuthProvider.credentialFromError(error);
-                // ...
             });
     }
 
